03916e6cbcb0967805c8fd34831548c22ef50167
BALUSU SIVA HARSHITHA <158086009+Siva-Harshitha@users.noreply.github.com>
2024-08-25 16:28:52 +0530

    RegisterDAO.java

--- RegisterDAO.java
+++ RegisterDAO.java
@@ -5,15 +5,10 @@ public class RegisterDAO {
 	public int register(UserBean ub) {
 		try {
 			Connection con=DBConnection.getCon();
-			/*PreparedStatement ps=con.prepareStatement("insert into UserReg59 values(?,?,?,?,?,?,?)");*/
 			PreparedStatement ps=con.prepareStatement("insert into UserReg59 values(?,?,?)");
 			ps.setString(1, ub.getuName());
 			ps.setString(2, ub.getpWord());
 			ps.setString(3, ub.getfName());
-			/*ps.setString(4, ub.getlName());
-			ps.setString(5, ub.getAddr());
-			ps.setString(6, ub.getmId());
-			ps.setLong(7, ub.getPhNo());*/
 			k=ps.executeUpdate();
 		}
 		catch(Exception e)
