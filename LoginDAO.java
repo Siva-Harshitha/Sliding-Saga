f47e60a1ae98ecc2d7ef862ac43eb4872626ad9e
BALUSU SIVA HARSHITHA <158086009+Siva-Harshitha@users.noreply.github.com>
2024-08-25 16:20:41 +0530

    LoginDAO.java

--- LoginDAO.java
+++ LoginDAO.java
@@ -1,35 +1,4 @@
-/*
-package test;
-import java.sql.*;
-public class LoginDAO {
-	public UserBean ub=null;
-	public UserBean login(String uN,String pW) {
-		try
-		{
-			Connection con=DBConnection.getCon();
-			PreparedStatement ps=con.prepareStatement("select * from UserReg59 where uname=? and pword=?");
-			ps.setString(1, uN);
-			ps.setString(2, pW);
-			ResultSet rs=ps.executeQuery();
-			if(rs.next())
-			{
-				ub=new UserBean();
-				ub.setuName(rs.getString(1));
-				ub.setpWord(rs.getString(2));
-				ub.setfName(rs.getString(3));
-				ub.setlName(rs.getString(4));
-				ub.setAddr(rs.getString(5));
-				ub.setmId(rs.getString(6));
-				ub.setPhNo(rs.getLong(7));
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		return ub;
-	}
-}
-*/
+
 package test;
 import java.sql.*;
 
@@ -48,7 +17,6 @@ public class LoginDAO {
                 ub.setuName(rs.getString(1));
                 ub.setpWord(rs.getString(2));
                 ub.setfName(rs.getString(3));
-                /*ub.setlName(rs.getString(4));
                 ub.setAddr(rs.getString(5));
                 ub.setmId(rs.getString(6));
                 ub.setPhNo(rs.getLong(7));*/
